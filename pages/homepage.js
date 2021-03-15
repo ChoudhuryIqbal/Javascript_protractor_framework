bee1ce2d2de82b7e208e56ce0501ebcaeecad3d9
Choudhury Iqbal <choudhuryiqbal@Choudhurys-MacBook-Air.local>
2021-03-15 10:34:41 -0400

    homepage

---
+++ pages/homepage.js
@@ -0,0 +1,28 @@
+let homepage=function ()
+{
+    let firstNumber_input=element (by.model('first') );
+    let secondNumber_input= element(by.model('second'));
+    let submit_btn=element(by.id('gobutton'));
+    let result_txt=element(by.css('.ng-binding'));
+
+    this.enterFirstNumber=function (first){
+        firstNumber_input.sendKeys(fist);
+
+    };
+
+    this.enterSecondNumber=function(second){
+        secondNumber_input.sendKeys(second);
+    }
+    this.enterSubmitBtn=function(){
+        submit_btn.click();
+    }
+
+    this.getResult=function(){
+        return result_txt.getText();
+    }
+
+
+    this.verifyResult=function(actual,expected){
+        expect (actual).toBe(expected);
+    }
+}
